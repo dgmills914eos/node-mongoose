785de027aba00daf2ce545fbad406d9bfc075444
Dan Mills <dgmills914eos@gmail.com>
2020-06-13 04:27:22 -0500

    pt2

--- models/campsite.js
+++ models/campsite.js
@@ -1,6 +1,28 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const commentSchema = new Schema({
+    rating: {
+        type: Number,
+        min: 1,
+        max: 5,
+        required: true
+    },
+    text: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
+});
+
 const campsiteSchema = new Schema({
     name: {
         type: String,
@@ -10,10 +32,12 @@ const campsiteSchema = new Schema({
     description: {
         type: String,
         required: true
-    }
+    },
+    comments: [commentSchema]
 }, {
     timestamps: true
 });
+
 
 const Campsite = mongoose.model('Campsite', campsiteSchema);
 
